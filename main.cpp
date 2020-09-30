29066bf19b4b0fe0e2c1410e77c357bad0bfb9b4
Shawn Hu <wwxyz.9999@gmail.com>
2020-09-29 23:14:35 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,37 +1,46 @@
 #include <glad/glad.h>
 #include <GLFW\glfw3.h>
 #include <iostream>
+#include <cmath>
 
-int main();
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
 
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 330 core\n"
-	"layout (location = 0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
-	"out vec4 FragColour;\n"
-	"void main()\n"
-	"{\n"
-	"	FragColour = vec4(0.8f, 0.4f, 0.0f, 1.0f);\n"
-	"}\0";
+"out vec4 FragColour;\n"
+"void main()\n"
+"{\n"
+"	FragColour = vec4(0.8f, 0.4f, 0.0f, 1.0f);\n"
+"}\0";
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
+int main();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+class Wall
+{
+public:
+	float width;
+	float height;
+	float x1, x2;
+	float y1, y2;
+};
+
+class UnitVector
+{
+public:
+	float x;
+	float y;
+	float normal;
+};
 
 int main() {
 
@@ -97,20 +106,46 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	//VERTICES OF SHAPE
-	float vertices[]{
-		-0.5f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-		 0.5f, -1.0f, 0.0f
+	Wall wall;
+	wall.width = 10;
+	wall.height = 2;
+	wall.x1 = -5;
+	wall.x2 = 5;
+	wall.y1 = 4;
+	wall.y2 = 4;
+
+	UnitVector camera_left;
+	camera_left.normal = sqrt(wall.x1 * wall.x1 + wall.y1 * wall.y1);
+	camera_left.x = wall.x1/camera_left.normal;
+	camera_left.y = wall.y1/camera_left.normal;
+	UnitVector camera_right;
+	camera_right.normal = sqrt(wall.x2 * wall.x2 + wall.y2 * wall.y2);
+	camera_right.x = wall.x2/camera_right.normal;
+	camera_right.y = wall.y2/camera_right.normal;
+
+	//VERTICES OF WALL
+	float vertices[] {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,	0.5f, 0.0f
+	};
+
+	unsigned int indices[] {
+		0,1,3,
+		1,2,3
 	};
 
 	//CREATING VERTEX BUFFER OBJECT
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STREAM_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -118,20 +153,57 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
+	double currentTime;
+	double lastTime = glfwGetTime();
+	double deltaT;
 	while (!glfwWindowShouldClose(WINDOW))
 	{
+		currentTime = glfwGetTime();
+		deltaT = currentTime - lastTime;
+		std::cout << deltaT << std::endl;
+		lastTime = currentTime;
 		processInput(WINDOW);
-		
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		
+		glClear(GL_COLOR_BUFFER_BIT);
+		if (glfwGetKey(WINDOW, GLFW_KEY_A) == GLFW_PRESS) {
+			vertices[0] -= deltaT * 1.0f;
+			vertices[3] -= deltaT * 1.0f;
+			vertices[6] -= deltaT * 1.0f;
+			vertices[9] -= deltaT * 1.0f;
+		}
+		if (glfwGetKey(WINDOW, GLFW_KEY_D) == GLFW_PRESS) {
+			vertices[0] += deltaT * 1.0f;
+			vertices[3] += deltaT * 1.0f;
+			vertices[6] += deltaT * 1.0f;
+			vertices[9] += deltaT * 1.0f;
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(WINDOW);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
